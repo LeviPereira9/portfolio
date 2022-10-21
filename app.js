20c9d5466f4ce10b87275c89b3123d464505b6c2
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-10-21 20:46:40 -0300

    Sobre mim finalizado

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const miniMenu = document.querySelector("#miniMenu");
 const miniContent = document.querySelector("#miniMenu__content");
-const aboutName = document.querySelector(".about__top__container__title__name");
+const knowContent = document.querySelectorAll(".knowledge__content__info");
+const knowStart = document.querySelector(".knowledge__content__start");
+let knowStatus = false;
+
+
 
 miniMenu.addEventListener("click", ()=>{
     miniContent.classList.toggle("showContent");
-})
+});
+
+const changeContent = (index)=>{
+    knowStatus = true;
+    
+    if(knowStatus){
+        knowStart.classList.add("hideContent");
+    }
+    
+    for(c = 0; c < knowContent.length ; c++){
+        knowContent[c].classList.remove("showContent");
+        knowContent[c].classList.add("hideContent");
+    }
+
+    knowContent[index].classList.add("showContent");
+}
