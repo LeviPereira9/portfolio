a2d61bf249d08763bf3acfbc242872ba1f18a3c6
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-10-21 03:05:40 -0300

    About Top finalizado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const miniMenu = document.querySelector("#miniMenu");
 const miniContent = document.querySelector("#miniMenu__content");
+const aboutName = document.querySelector(".about__top__container__title__name");
 
 miniMenu.addEventListener("click", ()=>{
     miniContent.classList.toggle("showContent");
